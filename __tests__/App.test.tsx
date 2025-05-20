62f80dd3d27edb810c6b704bd9533bcf4fae87d1
lrios26 <161726300+lrios26@users.noreply.github.com>
2025-05-20 13:00:34 -0500

    Delete __tests__/App.test.tsx

--- __tests__/App.test.tsx
+++
@@ -1,9 +0,0 @@
-// App.test.tsx
-import React from 'react';
-import { render } from '@testing-library/react-native';
-import App from './App';
-
-test('renderiza correctamente el componente App', () => {
-  const { getByText } = render(<App />);
-  expect(getByText('Mood Journal')).toBeTruthy(); // Asegúrate de que este texto exista
-});
